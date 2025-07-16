9b8106353878eaa06354596290139d74264eea77
Schizy <ali.mechichi@gmail.com>
2025-07-16 22:57:46 +0200

    Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,24 @@
+import './Header.css'
+
 function Header() {
-    return <header>Coucou</header>
+    return <header>
+        <div className="container">
+            <div className="header-content">
+                <div className="logo">
+                    <img src="torii_japonais.svg" alt="Torii" /> MyJProgress
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#accueil">Accueil</a></li>
+                        <li><a href="#cours">Cours</a></li>
+                        <li><a href="#progres">Progrès</a></li>
+                        <li><a href="#communaute">Communauté</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
 }
 
 export default Header;
