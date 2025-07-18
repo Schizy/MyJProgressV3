7fd855218c60d6f9fd1f334e3a0234936b7a008f
Schizy <ali.mechichi@gmail.com>
2025-07-18 16:10:19 +0200

    use real api in progress component

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ProgressCard from '../ProgressCard/ProgressCard';
 import './Progress.css';
 
 const Progress = () => {
 
     const [progressCards, setProgressCards] = useState([
-        { label: 'Kanji appris', number: '1 247' },
-        { label: 'Niveau N3', number: '85%' },
-        { label: 'Jours consécutifs', number: '42' },
-        { label: 'Points XP', number: '156' },
+        // { label: 'Kanji appris', number: '1 247' },
+        // { label: 'Niveau N3', number: '85%' },
+        // { label: 'Jours consécutifs', number: '42' },
+        // { label: 'Points XP', number: '156' },
     ])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/progress_cards')
+            .then(response => response.json())
+            .then(data => setProgressCards(data))
+            .catch(error => console.error('Error fetching progress cards:', error));
+    }, []);
 
     return (
         <section className="progress-section">
