3389cb8ce818808692cd0c4573baa0460b35ddfd
Schizy <ali.mechichi@gmail.com>
2025-07-18 15:11:25 +0200

    Makes FeatureCard use real api

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,17 +1,25 @@
+import { useEffect, useState } from 'react';
 import FeatureCard from '../FeatureCard/FeatureCard';
 import './Features.css';
 
 const Features = () => {
 
-    const features = [
-        {icon: '漢', title: 'Kanji Interactifs', description: 'Apprenez les caractères japonais avec des exercices interactifs et des mnémotechniques efficaces.'},
-        {icon: '話', title: 'Conversation', description: 'Pratiquez la conversation avec des dialogues réalistes et des simulations de situations quotidiennes.'},
-        {icon: '文', title: 'Grammaire', description: 'Maîtrisez la grammaire japonaise avec des leçons structurées et des exercices progressifs.'}
-    ]
+    const [featureCards, setFeatureCards] = useState([
+        // {icon: '漢', title: 'Kanji Interactifs', description: 'Apprenez les caractères japonais avec des exercices interactifs et des mnémotechniques efficaces.'},
+        // {icon: '話', title: 'Conversation', description: 'Pratiquez la conversation avec des dialogues réalistes et des simulations de situations quotidiennes.'},
+        // {icon: '文', title: 'Grammaire', description: 'Maîtrisez la grammaire japonaise avec des leçons structurées et des exercices progressifs.'}
+    ]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/feature_cards')
+            .then(response => response.json())
+            .then(data => setFeatureCards(data))
+            .catch(error => console.error('Error fetching feature cards:', error));
+    }, []);
 
     return (
         <section className="features">
-            {features.map(featureCard => <FeatureCard key={featureCard.icon} {...featureCard} />)}
+            {featureCards.map(featureCard => <FeatureCard key={featureCard.icon} {...featureCard} />)}
         </section>
     )
 }
