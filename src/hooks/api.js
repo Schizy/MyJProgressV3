c6611c497fbf03d9ebd3e974c98ce9e2fbcc1f0e
Schizy <ali.mechichi@gmail.com>
2025-07-22 23:58:09 +0200

    Adds ttl to cache - wip

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,5 +1,11 @@
 const API_URL = 'http://localhost:8000/api'
 
+// TODO: incorporer le cache ici
+// Au lieu d'utiliser le local storage state dans le component
+// Ca doit etre transparent
+// Je local storage la data + un key_ttl
+// Avec Date.now() + ttl
+// Et à chaque re-render je check Date.now() < timestamp
 async function fetchJson(path) {
     const response = await fetch(API_URL + path)
 
