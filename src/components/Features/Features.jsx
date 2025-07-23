df48f9e40fda4022054162df107e17a4de7a6ec1
Schizy <ali.mechichi@gmail.com>
2025-07-23 13:10:31 +0200

    moved the cache logic in the api call with a ttl

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -2,22 +2,19 @@ import { useEffect, useState } from 'react';
 import FeatureCard from '../FeatureCard/FeatureCard';
 import './Features.css';
 import { getFeatureCards } from '../../utils/api';
-import useStoredState from '../../hooks/useStoredState';
 
 const Features = () => {
 
-    const [featureCards, setFeatureCards] = useStoredState('featuresCards', [
+    const [featureCards, setFeatureCards] = useState([
         // {icon: '漢', title: 'Kanji Interactifs', description: 'Apprenez les caractères japonais avec des exercices interactifs et des mnémotechniques efficaces.'},
         // {icon: '話', title: 'Conversation', description: 'Pratiquez la conversation avec des dialogues réalistes et des simulations de situations quotidiennes.'},
         // {icon: '文', title: 'Grammaire', description: 'Maîtrisez la grammaire japonaise avec des leçons structurées et des exercices progressifs.'}
     ]);
 
     useEffect(() => {
-        if (!featureCards.length) {
-            getFeatureCards()
-                .then(data => setFeatureCards(data))
-                .catch(error => console.error('Error fetching feature cards:', error));
-        }
+        getFeatureCards()
+            .then(data => setFeatureCards(data))
+            .catch(error => console.error('Error fetching feature cards:', error));
     }, []);
 
     return (
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,32 @@
+import { getItem, setItem } from "./localStorage"
+
 const API_URL = 'http://localhost:8000/api'
 const TTL = 5 * 60 * 1000 // 5min
 
+async function apiCall(path) {
 
-// TODO: incorporer le cache ici
-// Au lieu d'utiliser le local storage state dans le component
-// Ca doit etre transparent
-// Je local storage la data + un key_ttl
-// Avec Date.now() + ttl
-// Et à chaque re-render je check Date.now() < timestamp
-async function fetchJson(path) {
+    const cachedResponse = getItem(path)
+    const cacheTTL = getItem(path + '_ttl')
+
+    // Cache hit
+    if (cachedResponse && cacheTTL > Date.now()) {
+        return cachedResponse
+    }
+
+    // Cache miss
     const response = await fetch(API_URL + path)
+    const data = await response.json()
 
-    return await response.json()
+    setItem(path, data)
+    setItem(path + '_ttl', Date.now() + TTL)
+
+    return data
 }
 
 export const getProgressCards = async () => {
-    return fetchJson('/progress_cards')
+    return apiCall('/progress_cards')
 }
 
 export const getFeatureCards = async () => {
-    return fetchJson('/feature_cards')
+    return apiCall('/feature_cards')
 }
