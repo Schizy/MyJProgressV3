b64120bf27475f016c2c0b4d82d8fa8c41b37b64
Schizy <ali.mechichi@gmail.com>
2025-07-31 23:48:05 +0200

    adds default user in migration

--- back/migrations/Version20250720121818.php
+++ back/migrations/Version20250720121818.php
@@ -23,6 +23,8 @@ final class Version20250720121818 extends AbstractMigration
         $this->addSql('CREATE TABLE feature_card (id INT AUTO_INCREMENT NOT NULL, icon VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE progress_card (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, number VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+
+        $this->addSql('INSERT INTO `user` VALUES (1, "Schizy","[]", "$2y$13$u9s7PPOUAgDG7TJcR3PXGeP42UGEoWBSCMYTD3mz09f4aSUx9auPu")');
     }
 
     public function down(Schema $schema): void
